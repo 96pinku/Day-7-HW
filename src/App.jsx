dd6f2f6e51b09f6157f6b81faa6cffd641564c0a
Pinku <pinkuparmer96@gmail.com>
2024-07-18 23:57:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Resturants from './components/Resturants/Resturants'
+
+function App() {
+  return (
+    <>
+    <Resturants/>
+    </>
+  )
+}
+
+export default App
